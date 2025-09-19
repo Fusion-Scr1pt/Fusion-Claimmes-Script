408ff895db48c509bf3ca5413a31ef40be366a22
Fusion Development <ilano1312@hotmail.com>
2025-09-19 22:21:05 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,9 +1,8 @@
 Config = {}
 
--- For /claimmes
 Config.EnableClaimWebhook = true
 Config.ClaimWebhookURL = "https://discord.com/api/webhooks/1361435181298483310/ClaimWebhook"
 
--- For /resetmes
 Config.EnableResetWebhook = true
+
 Config.ResetWebhookURL = "https://discord.com/api/webhooks/1361435181298483310/ResetWebhook"
